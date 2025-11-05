59400d34e8a501e934c4096f363c24d739a2d31f
Peiyao Wang <46548390+peiyao-w@users.noreply.github.com>
2025-11-04 22:34:11 -0500

    Add files via upload

--- action_segmentation/__init__.py
+++ action_segmentation/__init__.py
@@ -1,9 +1,36 @@
-from .config import get_default_config,  update_config, configurable
-from .collators import create_collator
-from .datasets import create_dataset, create_dataloader, augment_crop
-from .models import apply_data_parallel_wrapper, create_model, SetCriterion, SetCriterion_box, SetCriterion_bd, SetCriterion_cat_bd, SetCriterion_trans_bd, SetCriterion_maskw
-from .losses import create_loss, softmax_focal_loss
-from .optim import create_optimizer, create_set_optimizer,create_part_optimizer,create_regression_optimizer
-from .scheduler import create_scheduler, create_set_scheduler
+import importlib
 
-import action_segmentation.utils
+import torch
+import torch.nn as nn
+import torch.distributed as dist
+import yacs.config
+from .criterion import SetCriterion,  sigmoid_focal_loss
+from .criterion_box import SetCriterion_box
+from .criterion_bd import SetCriterion_bd
+from .criterion_maskw import SetCriterion_maskw
+from .criterion_cat_bd import SetCriterion_cat_bd
+from .criterion_trans_bd import SetCriterion_trans_bd
+
+def create_model(config: yacs.config.CfgNode) -> nn.Module:
+    module = importlib.import_module(
+        'action_segmentation.models'
+        f'.{config.model.name}')
+    model = getattr(module, 'Network')(config) #get model class
+    device = torch.device(config.device)
+    model.to(device)
+    return model
+
+
+def apply_data_parallel_wrapper(config: yacs.config.CfgNode,
+                                model: nn.Module) -> nn.Module:
+    local_rank = config.train.dist.local_rank
+    if dist.is_available() and dist.is_initialized():
+        if config.train.dist.use_sync_bn:
+            model = nn.SyncBatchNorm.convert_sync_batchnorm(model)
+        model = nn.parallel.DistributedDataParallel(model,
+                                                    device_ids=[local_rank],
+                                                    output_device=local_rank,
+                                                    find_unused_parameters=True)
+    else:
+        model.to(config.device)
+    return model
